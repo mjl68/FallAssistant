342d464d77d16a368a279b5d30723be78c99a009
MJL68 <mjl68@txstate.edu>
2015-04-26 10:14:24 -0500

    Changed: updated sentSmsByManager

--- Application/build/generated/source/r/debug/com/falldetect2015/android/fallassistant/R.java
+++ Application/build/generated/source/r/debug/com/falldetect2015/android/fallassistant/R.java
@@ -1657,16 +1657,17 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09003d;
         public static final int action_mode_bar_stub=0x7f09003c;
         public static final int action_mode_close_button=0x7f090025;
-        public static final int action_websearch=0x7f090059;
+        public static final int action_websearch=0x7f090057;
         public static final int activity_chooser_view_content=0x7f090026;
         public static final int alertTitle=0x7f090030;
         public static final int always=0x7f090016;
         public static final int beginning=0x7f090013;
-        public static final int button=0x7f090056;
-        public static final int button2=0x7f090058;
+        public static final int button=0x7f090052;
+        public static final int button2=0x7f090054;
         public static final int buttonPanel=0x7f090036;
         public static final int checkbox=0x7f090038;
         public static final int collapseActionView=0x7f090017;
+        public static final int contact_options_cb=0x7f090055;
         public static final int contentPanel=0x7f090031;
         public static final int content_frame=0x7f090050;
         public static final int custom=0x7f090035;
@@ -1711,6 +1712,7 @@ containing a value of this type.
         public static final int search_src_text=0x7f090049;
         public static final int search_voice_btn=0x7f09004d;
         public static final int select_dialog_listview=0x7f09004e;
+        public static final int settings_wait_timeout_spinner=0x7f090056;
         public static final int shortcut=0x7f090039;
         public static final int showCustom=0x7f09000f;
         public static final int showHome=0x7f090010;
@@ -1722,16 +1724,12 @@ containing a value of this type.
         public static final int submit_area=0x7f09004b;
         public static final int tabMode=0x7f09000b;
         public static final int textSpacerNoButtons=0x7f090033;
-        public static final int textView=0x7f090057;
+        public static final int textView=0x7f090053;
         public static final int title=0x7f09002c;
         public static final int title_template=0x7f09002f;
         public static final int topPanel=0x7f09002e;
         public static final int up=0x7f090008;
         public static final int useLogo=0x7f090012;
-        public static final int widget0=0x7f090052;
-        public static final int widget32=0x7f090053;
-        public static final int widget33=0x7f090054;
-        public static final int widget34=0x7f090055;
         public static final int withText=0x7f09001a;
         public static final int wrap_content=0x7f09001d;
     }
@@ -1770,12 +1768,12 @@ containing a value of this type.
         public static final int activity_navigation_drawer=0x7f03001a;
         public static final int drawer_list_item=0x7f03001b;
         public static final int fragment_options=0x7f03001c;
-        public static final int help=0x7f03001d;
-        public static final int help2=0x7f03001e;
-        public static final int sample_dashboard_item=0x7f03001f;
-        public static final int select_dialog_item_material=0x7f030020;
-        public static final int select_dialog_multichoice_material=0x7f030021;
-        public static final int select_dialog_singlechoice_material=0x7f030022;
+        public static final int help2=0x7f03001d;
+        public static final int sample_dashboard_item=0x7f03001e;
+        public static final int select_dialog_item_material=0x7f03001f;
+        public static final int select_dialog_multichoice_material=0x7f030020;
+        public static final int select_dialog_singlechoice_material=0x7f030021;
+        public static final int settings=0x7f030022;
         public static final int support_simple_spinner_dropdown_item=0x7f030023;
     }
     public static final class menu {
